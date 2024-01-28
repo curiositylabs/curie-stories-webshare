64cecf27ad21bca40cbba845d41791c8276b6efe
rahulyal <vyalavarthi@scu.edu>
2024-01-27 18:27:15 -0800

    Push

--- routes/story/[id].tsx
+++ routes/story/[id].tsx
@@ -5,4 +5,5 @@ export default async function StoryData(_req: any, ctx: FreshContext) {
     let response = await fetch('https://curie-backend-pwxppjv6ha-uw.a.run.app/stories/any-profile-id/' + ctx.params.id);
     let data = await response.json();
     return Showcase(data);
+    // push to deno deploy
 }
