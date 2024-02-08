6d20d381abc3be9b85007116cc02eb3f1d01907e
rahulyal <vyalavarthi@scu.edu>
2024-02-08 14:21:15 -0800

    Implemented Open Graph Protocol
    Implemented open graph protocol for link styling

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -5,8 +5,9 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>webshare</title>
+        <title>Story</title>
         <link rel="stylesheet" href="/styles.css" />
+        <link rel="icon" href="/favicon.ico" />
       </head>
       <body>
         <Component />
--- routes/story/[id].tsx
+++ routes/story/[id].tsx
@@ -2,9 +2,24 @@ import { FreshContext } from "$fresh/server.ts";
 import Showcase from "../../components/Showcase.tsx";
 
 export default async function StoryData(_req: any, ctx: FreshContext) {
-    let id = ctx.params.id
-    let response = await fetch('https://curie-backend-pwxppjv6ha-uw.a.run.app/stories/any-profile-id/' + id);
-    let data = await response.json();
-    return Showcase(data, id);
-    // push to deno deploy
+  let id = ctx.params.id;
+  let response = await fetch(
+    "https://curie-backend-pwxppjv6ha-uw.a.run.app/stories/any-profile-id/" +
+      id,
+  );
+  let data = await response.json();
+  return (
+    <html>
+      <head>
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={data.title} />
+        <meta property="og:image" content={data.cover} />
+        <meta property="og:description" content={data.description} />
+      </head>
+      <body>
+        {Showcase(data, id)}
+      </body>
+    </html>
+  );
+  // push to deno deploy
 }
