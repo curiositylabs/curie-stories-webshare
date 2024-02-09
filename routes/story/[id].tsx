89159755baaac9603a2dac0a2389e6070d5ed35c
rahulyal <vyalavarthi@scu.edu>
2024-02-08 18:51:07 -0800

    testing to fix open graph protocol

--- routes/story/[id].tsx
+++ routes/story/[id].tsx
@@ -14,7 +14,10 @@ export default async function StoryData(_req: any, ctx: FreshContext) {
         <meta property="og:type" content="website" />
         <meta property="og:title" content={data.title} />
         <meta property="og:image" content={data.cover} />
-        <meta property="og:description" content={data.description} />
+        <meta
+          property="og:url"
+          content={"https://stories.curiositylabs.ai/story/" + id}
+        />
       </head>
       <body>
         {Showcase(data, id)}
