1e0818a34b0c64d75a8472db392ac7ed6570e913
rahulyal <vyalavarthi@scu.edu>
2024-02-09 11:59:02 -0800

    og:image fix
    resizing for whatsapp

--- routes/story/[id].tsx
+++ routes/story/[id].tsx
@@ -13,8 +13,9 @@ export default async function StoryData(_req: any, ctx: FreshContext) {
       <head>
         <meta property="og:type" content="website" />
         <meta property="og:title" content={data.title} />
-        <meta property="og:image:secure_url" content={data.cover} />
-        <meta property="og:image" content={data.cover} />
+        <meta property="og:image/png" content={data.cover} />
+        <meta property="og:image:width" content="256" />
+        <meta property="og:image:height" content="256" />
         <meta
           property="og:url"
           content={"https://stories.curiositylabs.ai/story/" + id}
