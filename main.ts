fef0293f9ce42de0a4c58ae650d568d658444596
saketh-curi <saketh@curiositylabs.ai>
2024-05-26 23:57:26 -0500

    revert back to deno deploy from vercel

--- main.ts
+++ main.ts
@@ -4,17 +4,10 @@
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
 
+import "$std/dotenv/load.ts";
+
 import { start } from "$fresh/server.ts";
 import manifest from "./fresh.gen.ts";
 import config from "./fresh.config.ts";
 
-async function main() {
-  // Load environment variables in production
-  if (Deno.env.get("DENO_ENV") !== "development") {
-    await import("$std/dotenv/load.ts");
-  }
-
-  await start(manifest, config);
-}
-
-main();
+await start(manifest, config);
