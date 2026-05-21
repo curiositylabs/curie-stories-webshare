909a4073cb87ad6cbfe1670a71e9cbd92f5f08b7
Sekhar Chandra <sekhar@curiositylabs.ai>
2026-05-20 21:15:16 -0700

    GA4 integration

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,6 +1,8 @@
 import { type PageProps } from "$fresh/server.ts";
 import InstallBanner from "../islands/InstallBanner.tsx";
 
+const gaMeasurementId = "G-4X8NTYKBR9";
+
 export default function App({ Component }: PageProps) {
   return (
     <html>
@@ -10,8 +12,28 @@ export default function App({ Component }: PageProps) {
         <title>Story</title>
         <link rel="stylesheet" href="/styles.css" />
         <link rel="icon" href="/favicon.ico" />
+        {gaMeasurementId && (
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${gaMeasurementId}`}
+            >
+            </script>
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${gaMeasurementId}');
+                `,
+              }}
+            >
+            </script>
+          </>
+        )}
       </head>
-      <body className='bg-gray-100'>
+      <body className="bg-gray-100">
         <InstallBanner />
         <Component />
       </body>
